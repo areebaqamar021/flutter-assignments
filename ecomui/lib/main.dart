8c6e0ac267a1d917c8a6bd6aac4415ce7398f64c
areebaqamar021 <areebaqamar021@gmail.com>
2021-06-06 16:20:15 +0500

    merging

---
+++ ecomui/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Center(child: Text("Ecom App UI",
+          style: TextStyle(color: Colors.black,
+          fontWeight: FontWeight.bold))),
+
+
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.notifications),
+              color: Colors.black,
+              onPressed: (){
+
+              },), ],
+        ),
+
+        body: Home(),
+      ),
+    );
+  }
+}
